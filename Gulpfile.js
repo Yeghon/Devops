b95a1d1a85a749854dff28e22979a5bc94700e12
Yeghon <haronyegon90@gmail.com>
2021-09-27 19:53:38 +0300

    Rework gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,14 @@ const runSequence = require('run-sequence'); // Run tasks sequentially
 const jsonModify = require('gulp-json-modify');
 
 
-gulp.task('upversion', function () {
+gulp.task('upversion', function (release) {
   let currentVersion = require('./package.json').version
   console.log(`Current version: ${currentVersion}`)
   let splitVersion = currentVersion.split('.') // Format 1.prod.stage.dev
   let vBump = ''
   let index = 0
-  switch (process.argv[process.argv.length - 1]) {
+  console.log(process.argv[4])
+  switch (process.argv[4]) {
     case 'production':
       vBump = splitVersion[1].split('"')
       index = 1
