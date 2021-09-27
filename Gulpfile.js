3358c42dc197297d89d0630471cd8e45fbe99ee2
Yeghon <haronyegon90@gmail.com>
2021-09-27 21:45:50 +0300

    No major changes just some code cleanup here and there

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,65 +1,67 @@
-const gulp = require('gulp');
-const runSequence = require('run-sequence'); // Run tasks sequentially
-const jsonModify = require('gulp-json-modify');
+const gulp = require('gulp')
+const runSequence = require('run-sequence') // Run tasks sequentially
+const jsonModify = require('gulp-json-modify')
 
 // <<<<<<< HEAD
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //  AUTO UPDATE PATCH TASK
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-gulp.task('upversion', function () {
-    console.log(process.argv[4])
-    console.log("Version bump above")
-    let ver = require('./package.json').version; //version defined in the package.json file
-    console.log('current version: ', ver)
-    let splitString = ver.split('.', 3)
-    let patchVersion = splitString[2].split('"',1)
-    let patchNumber = Number(patchVersion[0])
-    patchNumber++
-    splitString[2] = String(patchNumber);
-    process.env.VERSION = splitString.join('.');
-    console.log(process.env.VERSION)
-// =======
+// gulp.task('upversion', function () {
+//     console.log(process.argv[4])
+//     console.log("Version bump above")
+//     let ver = require('./package.json').version; //version defined in the package.json file
+//     console.log('current version: ', ver)
+//     let splitString = ver.split('.', 3)
+//     let patchVersion = splitString[2].split('"',1)
+//     let patchNumber = Number(patchVersion[0])
+//     patchNumber++
+//     splitString[2] = String(patchNumber);
+//     process.env.VERSION = splitString.join('.');
+//     console.log(process.env.VERSION)
+// // =======
 
-// gulp.task('upversion', function (release) {
-//   let currentVersion = require('./package.json').version
-//   // console.log(process.argv)
-//   console.log(`Current version: ${currentVersion}`)
-//   let splitVersion = currentVersion.split('.') // Format 1.prod.stage.dev
-//   let vBump = ''
-//   let index = 0
-//   switch (release) {
-//     case 'production':
-//       vBump = splitVersion[1].split('"')
-//       index = 1
-//       break
-//     case 'staging':
-//       vBump = splitVersion[2].split('"')
-//       index = 2
-//       break
-//     case 'development':
-//       vBump = splitVersion[3].split('"')
-//       index = 3
-//       break
-//     default:
-//       break
-//   }
-//   let patch = Number(vBump)
-//   patch++
-//   splitVersion[index] = String(patch)
-//   process.env.VERSION = splitVersion.join('.')
-//   console.log(`New Version: ${process.env.VERSION}`)
-// >>>>>>> dae8bc8a787670d0864f2042e1e6f2390220d25a
-// })
+gulp.task('upversion', function (release) {
+  let currentVersion = require('./package.json').version
+  //   console.log(process.argv)
+  console.log(`Current version: ${currentVersion}`)
+  let splitVersion = currentVersion.split('.') // Format 1.prod.stage.dev
+  let vBump = ''
+  let index = 0
+  switch (release) {
+    case 'production':
+      vBump = splitVersion[1].split('"')
+      index = 1
+      break
+    case 'staging':
+      vBump = splitVersion[2].split('"')
+      index = 2
+      break
+    case 'development':
+      vBump = splitVersion[3].split('"')
+      index = 3
+      break
+    default:
+      break
+  }
+  let patch = Number(vBump)
+  patch++
+  splitVersion[index] = String(patch)
+  process.env.VERSION = splitVersion.join('.')
+  console.log(`New Version: ${process.env.VERSION}`)
+})
 
 gulp.task('saveversion', function () {
-return gulp.src(['./package.json'])
-    .pipe(jsonModify({
+  return gulp
+    .src(['./package.json'])
+    .pipe(
+      jsonModify({
         key: 'version',
-        value: process.env.VERSION
-    }))
+        value: process.env.VERSION,
+      }),
+    )
     .pipe(gulp.dest('./'))
 })
 
 gulp.task('autoversion', function () {
-    runSequence('upversion','saveversion');
+  runSequence('upversion', 'saveversion')
 })
